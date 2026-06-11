d1ad350e7a546f4b66bb27b56024d626b31bea60
sameer9860 <samirkhatiwada68@gmail.com>
2026-06-11 20:22:03 +0545

    feature(auth): implement token-based user authentication and data scoping

--- expenses/models.py
+++ expenses/models.py
@@ -1,18 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="categories")
+    name = models.CharField(max_length=100)
     description = models.CharField(max_length=255, blank=True)
 
     class Meta:
         verbose_name_plural = "categories"
+        unique_together = ("user", "name") # unique name for each user
 
     def __str__(self):
         return self.name
 
 
 class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="expenses")
     title = models.CharField(max_length=200)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(
