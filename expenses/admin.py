9bb441c86dab3f92a8c8135c897503ddf10d86e5
sameer9860 <samirkhatiwada68@gmail.com>
2026-06-12 14:23:22 +0545

    feature(optional): add expense search/filtering and CSV export

--- expenses/admin.py
+++ expenses/admin.py
@@ -4,5 +4,16 @@ from .models import Category, Expense
 
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Expense)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'monthly_limit')
+    list_filter = ('user',)
+    search_fields = ('name', 'description')
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'amount', 'category', 'date')
+    list_filter = ('user', 'category', 'date')
+    search_fields = ('title', 'notes', 'category__name')
+    date_hierarchy = 'date'
+    ordering = ('-date',)
